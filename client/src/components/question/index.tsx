3da3cfb9db70f95c99420adab38f3eef27ca8fe3
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-27 01:29:40 +0300

    question comp res
    question comp res

--- client/src/components/question/index.tsx
+++ client/src/components/question/index.tsx
@@ -9,14 +9,14 @@ interface Iprops {
 
 const QuestionsCont: React.FC<Iprops> = ({ name, img, question }) => {
   return (
-    <div className="flex h-20 w-4/5 items-center m-6 border">
-      <div className="flex flex-col items-center  flex-[1]">
+    <div className="flex items-center m-6 border border-cyan-700 w-full md:w-4/5 lg:w-3/4 xl:w-2/3 h-32">
+      <div className="flex h-full   justify-center flex-col items-center md:flex-col  md:items-center flex-[1]">
         <div>
           <img className="w-10 h-10 rounded-full" src={img} />
         </div>
         <div className="text-green-300">{name}</div>
       </div>
-      <div className="flex flex-[8] h-full justify-center  items-center">
+      <div className="flex flex-[8] h-full justify-center items-center">
         <span className="text-xl text-red-400">{question}</span>
       </div>
       <div className="flex justify-evenly flex-[1]">
