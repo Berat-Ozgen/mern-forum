77e8c7a34d7ac4d09297fc307d1bce8b002a1c3b
Berat Özgen <102038617+Berat-Ozgen@users.noreply.github.com>
2022-12-27 01:14:59 +0300

    Footer res
    Footer res

--- client/src/components/footer/Footer.tsx
+++ client/src/components/footer/Footer.tsx
@@ -2,41 +2,32 @@ import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <>
-      <footer className="p-4 bg-white border-t-2  border-gray-700 shadow md:px-6 md:py-8 dark:bg-gray-900">
-        <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="flex items-center mb-4 sm:mb-0">
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="mr-3 h-8"
-              alt="Flowbite Logo"
-            />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              brtForm
-            </span>
-          </div>
-          <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <div className="mr-4 hover:underline md:mr-6 ">About</div>
-            </li>
-            <li>
-              <div className="mr-4 hover:underline md:mr-6">Privacy Policy</div>
-            </li>
-            <li>
-              <div className="mr-4 hover:underline md:mr-6 ">Licensing</div>
-            </li>
-            <li>
-              <div className="hover:underline">Contact</div>
-            </li>
-          </ul>
+    <footer className="p-4 bg-white border-t-2 border-gray-700 shadow md:px-6 md:py-8 dark:bg-gray-900">
+      <div className="flex flex-col items-center md:flex-row md:items-start md:justify-between">
+        <div className="mb-4 md:mb-0">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            brtForm
+          </span>
         </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2022 <div className="hover:underline">Flowbite™</div>. All Rights
-          Reserved.
-        </span>
-      </footer>
-    </>
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 md:mb-0 dark:text-gray-400">
+          {["About", "Privacy Policy", "Licensing", "Contact"].map((item) => (
+            <li key={item}>
+              <div className="mr-4 hover:underline md:mr-6">{item}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm text-gray-500 md:text-center dark:text-gray-400">
+        © 2022 <div className="hover:underline">Flowbite™</div>. All Rights
+        Reserved.
+      </span>
+    </footer>
   );
 };
 
